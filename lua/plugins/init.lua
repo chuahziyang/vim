53e44877997ca54ccb6452187cfb2c4c9182f6d5
farhan-navas <farhanmnavas@gmail.com>
2024-05-05 01:34:42 +0800

    added tree-sitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
@@ -20,18 +20,26 @@ return {
   	"williamboman/mason.nvim",
   	opts = {
   		ensure_installed = {
-        "typescript-language-server","tailwindcss-language-server","eslint-lsp"
+        "typescript-language-server",
+        "tailwindcss-language-server",
+        "eslint-lsp",
+        "prettierd"
   		},
   	},
   },
   --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = function()
+      opts = require("nvchad.configs.treesitter")
+      opts.ensure_installed ={
+        "lua",
+        "typescript",
+        "javascript",
+        "tsx"
+      }
+      return opts
+
+    end
+  },
 }
