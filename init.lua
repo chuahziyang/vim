43be6e880815422691a83943fddbc6c43dcaba01
chuahziyang <czyang2002@gmail.com>
2024-07-24 03:05:41 +0800

    replace leap with flash

--- init.lua
+++ init.lua
@@ -36,17 +36,31 @@ require("lazy").setup({
     },
 
     {
-    "ggandor/leap.nvim",
-    enabled = true,
-    config = function(_, opts)
-    local leap = require("leap")
-    for k, v in pairs(opts) do
-      leap.opts[k] = v
-    end
-    leap.add_default_mappings(true)
-  end,
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      ---@type Flash.Config
+      opts = {},
+      keys = {
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      },
+    }
 
-}
+  --   {
+  --   "ggandor/leap.nvim",
+  --   enabled = true,
+  --   config = function(_, opts)
+  --   local leap = require("leap")
+  --   for k, v in pairs(opts) do
+  --     leap.opts[k] = v
+  --   end
+  --   leap.add_default_mappings(true)
+  -- end,
+
+  --   }
 })
 
 local userprofile = os.getenv("USERPROFILE")
