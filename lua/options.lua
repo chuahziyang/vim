a8ae302bcbd0c459e9b2ccd21ec42139418b2bd5
chuahziyang <czyang2002@gmail.com>
2024-08-25 12:03:19 +0800

    fix

--- lua/options.lua
+++ lua/options.lua
@@ -8,7 +8,8 @@ vim.o.clipboard = "unnamedplus"
 vim.o.relativenumber = true
 
 -- Use Powershell Instead
-vim.o.shell = "powershell"vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+vim.o.shell = "powershell"
+vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
 vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
 vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
 vim.o.shellquote = ""
