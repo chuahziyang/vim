9a733bc340d1e00ad23dbc7e5ac016884e6d3fa4
chuahziyang <czyang2002@gmail.com>
2024-08-25 10:27:40 +0800

    added possible projects plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -108,6 +108,27 @@ return {
     }
   },
 
+  -- {
+  --   "coffebar/neovim-project",
+  --   opts = {
+  --     projects = { -- define project roots
+  --       "~/projects/*",
+  --       "~/.config/*",
+  --     },
+  --   },
+  --   init = function()
+  --     -- enable saving the state of plugins in the session
+  --     vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+  --   end,
+  --   dependencies = {
+  --     { "nvim-lua/plenary.nvim" },
+  --     { "nvim-telescope/telescope.nvim", tag = "0.1.4" },
+  --     { "Shatur/neovim-session-manager" },
+  --   },
+  --   lazy = false,
+  --   priority = 100,
+  -- },
+
   {
     'tpope/vim-fugitive',
     lazy = false,
