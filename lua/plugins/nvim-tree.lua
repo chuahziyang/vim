76b40a5d81666e1c44cf5d9fcb935d9b960ab8d9
chuahziyang <czyang2002@gmail.com>
2024-09-08 18:51:20 +0800

    fix nvimtree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,3 +1,18 @@
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+  -- custom mappings
+end
+
+-- pass to setup along with your other options
 return {
   {
     "nvim-tree/nvim-tree.lua",
@@ -14,6 +29,7 @@ return {
           ignore = false,
           timeout = 500,
         },
+        on_attach = my_on_attach
       }
       -- Merge default and custom options
       local final_options = vim.tbl_deep_extend("force", default_options, custom_options)
