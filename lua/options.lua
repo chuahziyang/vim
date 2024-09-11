f14d8bfe6a23850f6d13fd781bd9bbf7ab651ebf
chuahziyang <czyang2002@gmail.com>
2024-09-11 16:54:16 +0800

    test

--- lua/options.lua
+++ lua/options.lua
@@ -7,10 +7,12 @@ require "nvchad.options"
 vim.o.clipboard = "unnamedplus"
 vim.o.relativenumber = true
 
--- Use Powershell Instead
-vim.o.shell = "powershell"
-vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-vim.o.shellquote = ""
-vim.o.shellxquote = ""
+if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+  vim.o.shell = "powershell"
+  vim.o.shellcmdflag =
+  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
+end
