aa0ed3f9bb0727bbfe27f21d301592cde2d143d5
chuahziyang <czyang2002@gmail.com>
2024-09-17 16:30:10 +0800

    refac

--- lua/options.lua
+++ lua/options.lua
@@ -2,17 +2,18 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
-vim.o.clipboard = "unnamedplus"
-vim.o.relativenumber = true
+o.clipboard = "unnamedplus"
+o.relativenumber = true
+o.swapfile = false
 
 if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
-  vim.o.shell = "powershell"
-  vim.o.shellcmdflag =
+  o.shell = "powershell"
+  o.shellcmdflag =
   "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-  vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-  vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-  vim.o.shellquote = ""
-  vim.o.shellxquote = ""
+  o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  o.shellquote = ""
+  o.shellxquote = ""
 end
