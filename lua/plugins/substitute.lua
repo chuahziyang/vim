be74d1bc0e0e3092c9c4f0b381a61bcba5f40952
chuahziyang <czyang2002@gmail.com>
2024-09-24 02:05:43 +0800

    substitute

--- lua/plugins/substitute.lua
+++ lua/plugins/substitute.lua
@@ -1,6 +1,7 @@
 return {
   {
     "gbprod/substitute.nvim",
+    lazy = false,
     opts = {
       highlight_substituted_text = {
         enabled = false,
@@ -8,9 +9,9 @@ return {
     },
     config = function()
       require('substitute').setup({})
-      vim.keymap.set("n", "gs", require('substitute').operator, { noremap = true })
-      vim.keymap.set("n", "gss", require('substitute').line, { noremap = true })
-      vim.keymap.set("n", "gS", require('substitute').eol, { noremap = true })
+      vim.keymap.set("n", "gx", require('substitute').operator, { noremap = true })
+      vim.keymap.set("n", "gxx", require('substitute').line, { noremap = true })
+      vim.keymap.set("n", "gX", require('substitute').eol, { noremap = true })
       vim.keymap.set("x", "gx", require('substitute').visual, { noremap = true })
     end,
   },
