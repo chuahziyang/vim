4e9ec7bd6983c05c49582dda694bc845d458f3c8
chuahziyang <czyang2002@gmail.com>
2024-09-23 13:28:55 +0800

    much better

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,10 +1,24 @@
-require "nvchad.configs.cmp"
-
 return {
   'hrsh7th/nvim-cmp',
   opts = function()
+    local cmp = require("cmp")
     local options = require("nvchad.configs.cmp")
-    table.insert(options.sources, 1, { name = 'supermaven' })
+
+    -- Disable automatic completion
+    -- Modify mapping to use Ctrl+Enter for completion
+    options.mapping = cmp.mapping.preset.insert({
+      ["<C-CR>"] = cmp.mapping.complete(),
+      -- Keep other existing mappings
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.close(),
+      ["<CR>"] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+      }),
+    })
+
+    -- Add your custom source
+
     return options
   end,
 }
