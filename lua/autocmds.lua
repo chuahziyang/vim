f429a1c7cd4d0b0c1e4a820fd7349cc0a58e063d
chuahziyang <czyang2002@gmail.com>
2025-01-31 23:49:41 +0800

    fix

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -15,3 +15,12 @@ vim.api.nvim_create_autocmd('CursorHold', {
   end,
   group = vim.api.nvim_create_augroup('HoverDiagnostic', { clear = true }),
 })
+
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
