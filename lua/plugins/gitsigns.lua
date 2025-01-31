8976873b6d0bdeec64b2b546fcd0daa277963c2e
chuahziyang <czyang2002@gmail.com>
2025-02-01 00:53:34 +0800

    asdasd

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,23 +5,23 @@ return {
       -- Load the default settings
       local default_options = require("nvchad.configs.gitsigns")
       -- Custom settings to override defaults
-      local custom_options = {
-        current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-        current_line_blame_opts = {
-          virt_text = true,
-          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-          delay = 400,
-          ignore_whitespace = false,
-        },
-        on_attach = function(bufnr)
-          default_options.on_attach(bufnr)
-          -- vim.keymap.del("n", "<leader>rh", { bufnr = bufnr, desc = "LSP Reset Hunk" })
-        end
-      }
-      -- Merge default and custom options
-      local final_options = vim.tbl_deep_extend("force", default_options, custom_options)
-      -- Apply the merged configuration
-      require("gitsigns").setup(final_options)
+      -- local custom_options = {
+      --   current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      --   current_line_blame_opts = {
+      --     virt_text = true,
+      --     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      --     delay = 400,
+      --     ignore_whitespace = false,
+      --   },
+      --   on_attach = function(bufnr)
+      --     default_options.on_attach(bufnr)
+      --     -- vim.keymap.del("n", "<leader>rh", { bufnr = bufnr, desc = "LSP Reset Hunk" })
+      --   end
+      -- }
+      -- -- Merge default and custom options
+      -- local final_options = vim.tbl_deep_extend("force", default_options, custom_options)
+      -- -- Apply the merged configuration
+      require("gitsigns").setup(default_options)
     end,
   },
 }
