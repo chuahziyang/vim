92c613c18798483bda8da0edba2e694da3fe8ede
chuahziyang <czyang2002@gmail.com>
2025-04-03 19:58:00 +0800

    sdf

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -8,14 +8,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
 })
 
-vim.api.nvim_create_autocmd('CursorHold', {
-  pattern = '*',
-  callback = function()
-    vim.diagnostic.open_float()
-  end,
-  group = vim.api.nvim_create_augroup('HoverDiagnostic', { clear = true }),
-})
-
 vim.api.nvim_create_autocmd("BufDelete", {
   callback = function()
     local bufs = vim.t.bufs
