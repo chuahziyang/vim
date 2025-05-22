74295a425495f5d5bba9ce00a8788b34d4a17cbc
ziyang <czyang2002@gmail.com>
2025-05-23 00:25:57 +0800

    fixes conform errors

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,24 +1,15 @@
 local options = {
-  -- formatters_by_ft = {
-  --   lua = { "stylua" },
-  --   -- css = { "prettier" },
-  --   -- html = { "prettier" },
-  -- },
   formatters_by_ft = {
-    javascript = { { "prettierd", "prettier" } },
-    typescript = { { "prettierd", "prettier" } },
-    javascriptreact = { { "prettierd", "prettier" } },
-    typescriptreact = { { "prettierd", "prettier" } },
-    json = { { "prettierd", "prettier" } },
-    graphql = { { "prettierd", "prettier" } },
-    markdown = { { "prettierd", "prettier" } },
-    css = { { "prettierd", "prettier" } },
-    scss = { { "prettierd", "prettier" } },
-  },
-  formatters = {
-    lua = {
-      command = "./../../../../Local/nvim-data/mason/bin/stylua.cmd"
-    }
+    lua = { "stylua" },
+    javascript = { "prettierd", "prettier", stop_after_first = true },
+    typescript = { "prettierd", "prettier", stop_after_first = true },
+    javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+    json = { "prettierd", "prettier", stop_after_first = true },
+    graphql = { "prettierd", "prettier", stop_after_first = true },
+    markdown = { "prettierd", "prettier", stop_after_first = true },
+    css = { "prettierd", "prettier", stop_after_first = true },
+    scss = { "prettierd", "prettier", stop_after_first = true },
   },
 
   format_on_save = {
