6f4cc60e45e804f9b6aac3b50766b3cd397c7a17
chuahziyang <czyang2002@gmail.com>
2025-06-06 07:37:30 +0800

    working

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,16 +1,37 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    typescript = { "prettierd", "prettier", stop_after_first = true },
-    javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-    json = { "prettierd", "prettier", stop_after_first = true },
-    graphql = { "prettierd", "prettier", stop_after_first = true },
-    markdown = { "prettierd", "prettier", stop_after_first = true },
-    css = { "prettierd", "prettier", stop_after_first = true },
-    scss = { "prettierd", "prettier", stop_after_first = true },
+  formatters = {
+    lua = {
+      command = "./../../../../Local/nvim-data/mason/bin/stylua.cmd"
+    },
+    javascript = {
+      command = "./../../../../Local/nvim-data/mason/bin/prettierd.cmd"
+    },
+    typescript = {
+      command = "./../../../../Local/nvim-data/mason/bin/prettierd.cmd"
+    },
+    javascriptreact = {
+      command = "./../../../../Local/nvim-data/mason/bin/prettierd.cmd"
+    },
+    typescriptreact = {
+      command = "./../../../../Local/nvim-data/mason/bin/prettierd.cmd"
+    },
+    html = {
+      command = "prettierd"
+    },
   },
+  -- formatters_by_ft = {
+  --   -- lua = { "stylua" },
+  --   javascript = { "prettierd", "prettier", stop_after_first = true },
+  --   typescript = { "prettierd", "prettier", stop_after_first = true },
+  --   javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+  --   typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+  --   json = { "prettierd", "prettier", stop_after_first = true },
+  --   graphql = { "prettierd", "prettier", stop_after_first = true },
+  --   markdown = { "prettierd", "prettier", stop_after_first = true },
+  --   css = { "prettierd", "prettier", stop_after_first = true },
+  --   scss = { "prettierd", "prettier", stop_after_first = true },
+  -- },
+
 
   format_on_save = {
     -- These options will be passed to conform.format()
@@ -18,5 +39,4 @@ local options = {
     lsp_fallback = true,
   },
 }
-
 require("conform").setup(options)
